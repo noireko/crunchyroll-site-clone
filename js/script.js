8c05afcfc9b2c29708fca6c74e0e95a4ef557649
noireko <agustinfernandezrolon@gmail.com>
2025-10-29 16:36:33 -0300

    Modified carrousel functionality

--- js/script.js
+++ js/script.js
@@ -38,6 +38,32 @@ carousels.forEach(carousel => {
     requestAnimationFrame(updateActiveItems);
   });
 
-  // inicializar al cargar
   updateActiveItems();
 });
+
+const carousel = document.querySelector('.container-music-videos');
+const slides = document.querySelectorAll('.music-slide');
+
+function updateActiveSlides() {
+    const rect = carousel.getBoundingClientRect();
+
+    slides.forEach(slide => {
+        const slideRect = slide.getBoundingClientRect();
+        const slideCenter = slideRect.left + slideRect.width / 2;
+        const carouselCenter = rect.left + rect.width / 2;
+
+        const distance = Math.abs(carouselCenter - slideCenter);
+        const threshold = slideRect.width / 2;
+
+        if (distance < threshold) {
+            slide.classList.add('active');
+        } else {
+            slide.classList.remove('active');
+        }
+    });
+}
+
+carousel.addEventListener('scroll', () => {
+    requestAnimationFrame(updateActiveSlides);
+});
+updateActiveSlides();
